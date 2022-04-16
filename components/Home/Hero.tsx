0d429bf00a044bf79b012b9f933b2dda27fca4ee
Ryhann <rayhancoding1603@gmail.com>
2022-04-16 12:25:05 +0700

    update animation - hero

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,5 +1,5 @@
 import gsap from 'gsap'
-import Reactfrom 'react'
+import React from 'react'
 
 export default function Hero() {
   return (
@@ -9,7 +9,7 @@ export default function Hero() {
       {/* Content Hero */}
       <div className="content-hero">
         <h1 className='text-[40px] sm:text-5xl lg:text-7xl font-bold text-white md:text-emerald-200 '><span className='text-green-300 mb:text-yellow-200'> Marmosets  </span>are one of the smallest primates in the world</h1>
-        <h1 className='text-2xl font-normal my-8 text-white md:text-emerald-100' ref={el => {typing = el}}>  <span className='text-teal-200'>their way of life is unique</span> and <span className='text-purple-100'>rare.</span>  </h1>
+        <h1 className='text-2xl font-normal my-8 text-white md:text-emerald-100'>  <span className='text-teal-200'>their way of life is unique</span> and <span className='text-purple-100'>rare.</span>  </h1>
       </div>
 
     </div>
