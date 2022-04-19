a4dac7e1893414a4f4a66d7bd3c0fd2e65ca1a72
= <=>
2022-04-19 18:56:27 +0700

    update data roadmap

--- pages/roadmap/index.tsx
+++ pages/roadmap/index.tsx
@@ -13,49 +13,49 @@ import Layout from '../../components/Roadmap/Layout'
      ]     
   },
 
-  // {
-  //    title : 'Phase 2 Grow up',
-  //    content : 
-  //    `- Will continue launch 125 NFT 
-  //    - Marketing with local celebrities 
-  //    - A portion of the sales produceed will be used to purchases and support benefits community nft
-  //    - Top collectors will get special edition 
-  //    - 100 NFT sold will proceed to phase 3
-  //    `
-  // },
+  {
+     title : 'Phase 2 Grow up',
+     content :  [
+       '- Will continue launch 125 NFT ',
+       '- Marketing with local celebrities ',
+       '- A portion of the sales produceed will be used to purchases and support benefits community nft',
+       '- Top collectors will get special edition' ,
+       '- 100 NFT sold will proceed to phase 3',
+     ]
+  },
 
-  // {
-  //    title : 'Phase 3  invasion',
-  //    content : 
-  //    `- Will continue lunch 500 NFT 
-  //    - T-shirt special edition for holder 
-  //    - changed nft design edition from 2D to 3D
-  //    - Creating community events
-  //    - Collaboration with more local artist
-  //    - 350 NFT sold will proceed to phase 4
-  //    `
-  // },
-  // {
-  //    title : 'Phase 4 Dominate',
-  //    content : 
-  //    `- Will continue launch 1500 NFT
-  //    - Partnership with celebrities 
-  //    - every purchase of our special nft can be customized for free
-  //    - Our NFT can be exchanged for food and drinks by collecting as many as 150 of our NFT items through places and drinks that we work with.
-  //    - More reward for holder (t-shirt special edition)
-  //    - 1000 NFT sold will proceed to phase 5
-  //    `
-  // },
-  // {
-  //    title : 'Phase 5 Final',
-  //    content : 
-  //    `- Will continue launch 1999
-  //    - Marketing with famous celebrities 
-  //    - utility to the greatest holder
-  //    - nft we will also make games and buy land in the metaverse
-  //    - help business people to promote their business through our 3D metaverse
-  //    `
-  // },
+  {
+     title : 'Phase 3  invasion',
+     content : [
+       '- Will continue lunch 500 NFT ',
+      '- T-shirt special edition for holder' ,
+       '- changed nft design edition from 2D to 3D',
+       '- Creating community events',
+       '- Collaboration with more local artist',
+       '- 350 NFT sold will proceed to phase 4',
+     ]
+  },
+  {
+     title : 'Phase 4 Dominate',
+     content : [
+       '- Will continue launch 1500 NFT',
+       '- Partnership with celebrities ',
+       '- every purchase of our special nft can be customized for free',
+       '- Our NFT can be exchanged for food and drinks by collecting as many as 150 of our NFT items through places and drinks that we work with.',
+       '- More reward for holder (t-shirt special edition)',
+       '- 1000 NFT sold will proceed to phase 5'
+     ] 
+  },
+  {
+     title : 'Phase 5 Final',
+     content : [
+       '- Will continue launch 1999',
+       '- Marketing with famous celebrities ',
+      '- utility to the greatest holder',
+       '- nft we will also make games and buy land in the metaverse',
+       '- help business people to promote their business through our 3D metaverse',
+     ] 
+  },
 ]
 
 roadmapData.join('<br />');
